f3fa617860c569692ac01aacbc74d732b61c4617
amaben2020 <amaben202phantom@gmail.com>
2021-06-27 13:21:42 +0100

    Enums

--- practice.js
+++ practice.js
@@ -1,4 +1,16 @@
 var addValues = function (a, b) {
     return a + b;
 };
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+if (Role.ADMIN === 0) {
+    console.log('Yes ooo');
+}
+else {
+    console.log('Na lie oooo');
+}
 console.log(addValues(1, 2));
--- practice.ts
+++ practice.ts
@@ -2,4 +2,16 @@ const addValues = (a: number, b: number) => {
 	return a + b;
 };
 
+enum Role {
+	ADMIN,
+	READ_ONLY,
+	AUTHOR,
+}
+
+if (Role.ADMIN === 0) {
+	console.log('Yes ooo');
+} else {
+	console.log('Na lie oooo');
+}
+
 console.log(addValues(1, 2));
